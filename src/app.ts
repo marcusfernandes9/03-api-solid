d8a752c7889697043f7cffd913fc1da051ea9302
Marcus Souza Fernandes <marcus.fernandes9@gmail.com>
2023-12-08 15:54:35 -0300

    Integração com Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify"
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Marcus Fernandes',
+    email: 'marcus.fernandes9@gmail.com',
+  },
+})
