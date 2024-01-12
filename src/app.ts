e4fff3013ae2156763dd1f0ba33c50298ee35920
Marcus Souza Fernandes <marcus.fernandes9@gmail.com>
2024-01-12 12:21:02 -0300

    gyms controllers

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
-import { appRoutes } from './http/controllers/users/routes'
+import { usersRoutes } from './http/controllers/users/routes'
 import fastifyJwt from '@fastify/jwt'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(gymsRoutes)
 app.setErrorHandler((error, _request, reply) => {
   if (error instanceof ZodError) {
     return reply
